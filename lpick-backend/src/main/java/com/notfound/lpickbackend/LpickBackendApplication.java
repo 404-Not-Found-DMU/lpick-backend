c34edc0ec04ca7b2717c5596d181aae6991dbcfe
JungUiJin <yealkki38@gmail.com>
2025-04-28 21:03:44 +0900

    setting : 초기 프로젝트 파일 세팅

---
+++ lpick-backend/src/main/java/com/notfound/lpickbackend/LpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notfound.lpickbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpickBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpickBackendApplication.class, args);
+    }
+
+}
