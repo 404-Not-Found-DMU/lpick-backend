322365ac6a6b2396fd3be506f0503f03b605c227
jakdalmak7961 <jakdalmak7961@gmail.com>
2025-07-27 20:04:42 +0900

    feat : command 관련 test 코드 추가

---
+++ lpick-backend/src/test/java/com/notfound/lpickbackend/userinfo/command/application/service/UserGearCommandServiceTest.java
@@ -0,0 +1,86 @@
+package com.notfound.lpickbackend.userinfo.command.application.service;
+
+import com.notfound.lpickbackend.common.exception.CustomException;
+import com.notfound.lpickbackend.common.exception.ErrorCode;
+import com.notfound.lpickbackend.servicedata.command.domain.Album;
+import com.notfound.lpickbackend.userinfo.command.application.domain.entity.UserAlbum;
+import com.notfound.lpickbackend.userinfo.command.application.domain.entity.UserGear;
+import com.notfound.lpickbackend.userinfo.command.application.domain.entity.UserInfo;
+import com.notfound.lpickbackend.userinfo.query.service.UserAlbumQueryService;
+import com.notfound.lpickbackend.userinfo.query.service.UserGearQueryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class UserGearCommandServiceTest {
+
+    @Mock
+    private UserGearQueryService userGearQueryService;
+
+    @InjectMocks
+    private UserGearCommandService userGearCommandService;
+
+
+    private UserGear mockUserGear;
+
+    private final String oauthId1 = "oauth123";
+    private final String oauthId2 = "oauth222";
+    private final String userGearId = "1";
+
+    @BeforeEach
+    void setUp() {
+        UserInfo mockUser1 = UserInfo.builder()
+                .oauthId(oauthId1) // 전치사로 OAuthType 추가
+                .nickname("")
+                .profile("")
+                .point(0)
+                .stackPoint(0)
+                .about("")
+                .lpti("")
+                .tier(null)
+                .build();
+
+        UserInfo mockUser2 = UserInfo.builder()
+                .oauthId(oauthId2) // 전치사로 OAuthType 추가
+                .nickname("")
+                .profile("")
+                .point(0)
+                .stackPoint(0)
+                .about("")
+                .lpti("")
+                .tier(null)
+                .build();
+
+
+        mockUserGear = UserGear.builder()
+                .userGearId(userGearId)
+                .eq(null)
+                .oauth(mockUser1)
+                .build();
+    }
+
+
+    @Test
+    void deleteUserGear() {
+        // User1의 userGear에 대해 접근
+        given(userGearQueryService.findById(userGearId)).willReturn(mockUserGear);
+
+        // 접근시도 인원은 User2므로 에러 발생
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            userGearCommandService.deleteUserGear(oauthId2, userGearId);
+        });
+
+
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.FORBIDDEN_RESOURCE_ACCESS);
+    }
+}
