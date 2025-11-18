e53308ef4fd504d47e7f7f64668c9891bdcba294
jakdalmak7961 <jakdalmak7961@gmail.com>
2025-11-18 17:42:54 +0900

    feat : ballot 여부 확인 api 추가

--- lpick-backend/src/main/java/com/notfound/lpickbackend/wiki/command/application/controller/BallotCommandController.java
+++ lpick-backend/src/main/java/com/notfound/lpickbackend/wiki/command/application/controller/BallotCommandController.java
@@ -1,7 +1,7 @@
 package com.notfound.lpickbackend.wiki.command.application.controller;
 
-import com.notfound.lpickbackend.common.exception.SuccessCode;
 import com.notfound.lpickbackend.security.details.OAuth2UserDetails;
+import com.notfound.lpickbackend.wiki.query.dto.response.BallotResponse;
 import com.notfound.lpickbackend.wiki.command.application.dto.request.BallotRequest;
 import com.notfound.lpickbackend.wiki.command.application.service.BallotCommandService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,12 +22,12 @@ public class BallotCommandController {
 
     @Operation(summary = "토론 투표", description = "로그인 필요. status가 VOTE인 상태인 debate에만 투표가능합니다. BallotValue는 다음과 같아야합니다 - AGREE, DISAGREE, ABSTAIN")
     @PostMapping("/debate/{debateId}/ballot")
-    public ResponseEntity<SuccessCode> ballotInDebate(
+    public ResponseEntity<BallotResponse> ballotInDebate(
             @PathVariable("debateId") String debateId,
             @RequestBody BallotRequest req,
             @AuthenticationPrincipal OAuth2UserDetails userDetail
     ) {
-        ballotCommandService.ballotToDebate(debateId, req, userDetail);
-        return ResponseEntity.ok(SuccessCode.SUCCESS);
+
+        return ResponseEntity.ok(ballotCommandService.ballotToDebate(debateId, req, userDetail));
     }
 }
